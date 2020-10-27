bdc2a4317575ec4460e14add28cc52d76fb0377a
JamesYaoWM <jtyao@email.wm.edu>
2020-10-26 20:54:00 -0400

    Create WinningActivity

--- app/src/main/java/edu/wm/cs/cs301/jamesyao/gui/WinningActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/jamesyao/gui/WinningActivity.java
@@ -1,4 +1,15 @@
 package edu.wm.cs.cs301.jamesyao.gui;
 
-public class WinningActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.wm.cs.cs301.jamesyao.R;
+
+public class WinningActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winning);
+    }
 }
