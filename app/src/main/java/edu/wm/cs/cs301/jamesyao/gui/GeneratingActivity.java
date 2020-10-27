86635844999cf8a894a0b0fc2581c0eb5520c82b
JamesYaoWM <jtyao@email.wm.edu>
2020-10-26 20:47:03 -0400

    Connected PlayManuallyActivity and PlayAnimationActivity to GeneratingActivity

--- app/src/main/java/edu/wm/cs/cs301/jamesyao/gui/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/jamesyao/gui/GeneratingActivity.java
@@ -2,13 +2,17 @@ package edu.wm.cs.cs301.jamesyao.gui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.wm.cs.cs301.jamesyao.R;
 
 public class GeneratingActivity extends AppCompatActivity {
+
+    public static final String MANUAL_MESSAGE = "edu.wm.cs.cs301.jamesyao.MANUAL";
+    public static final String ANIMATION_MESSAGE = "edu.wm.cs.cs301.jamesyao.ANIMATION";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +25,23 @@ public class GeneratingActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         //TextView textView = findViewById(R.id.textView);
         //textView.setText(message);
+    }
+
+    /** Called when the user taps the "Play Manually" button */
+    public void play_manually(View view) {
+        // do something in response to the button
+        Intent intent = new Intent(this, PlayManuallyActivity.class);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        intent.putExtra(MANUAL_MESSAGE, progressBar.getProgress());
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the "Play Animation" button */
+    public void play_animation(View view) {
+        // do something in response to the button
+        Intent intent = new Intent(this, PlayAnimationActivity.class);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        intent.putExtra(MANUAL_MESSAGE, progressBar.getProgress());
+        startActivity(intent);
     }
 }
