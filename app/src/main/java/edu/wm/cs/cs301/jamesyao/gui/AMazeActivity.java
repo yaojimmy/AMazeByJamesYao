2ed8badc46bfee66dfa6f8ce660f939284c6b046
JamesYaoWM <jtyao@email.wm.edu>
2020-10-26 20:12:47 -0400

    Created AMazeActivity

--- app/src/main/java/edu/wm/cs/cs301/jamesyao/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/jamesyao/gui/AMazeActivity.java
@@ -2,15 +2,29 @@ package edu.wm.cs.cs301.jamesyao.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import edu.wm.cs.cs301.jamesyao.R;
 
 public class AMazeActivity extends AppCompatActivity {
 
+    public static final String EXPLORE_MESSAGE = "edu.wm.cs.cs301.jamesyao.EXPLORE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mazeactivity_main);
+    }
+
+    /** Called when the user taps the "Send" button */
+    public void explore(View view) {
+        // do something in response to the button
+        Intent intent = new Intent(this, GeneratingActivity.class);
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        intent.putExtra(EXPLORE_MESSAGE, seekBar.getProgress());
+        startActivity(intent);
     }
 }
--- app/src/main/java/edu/wm/cs/cs301/jamesyao/gui/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/jamesyao/gui/GeneratingActivity.java
@@ -1,6 +1,8 @@
 package edu.wm.cs.cs301.jamesyao.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,14 @@ public class GeneratingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_generating);
+        setContentView(R.layout.activity_generating);
+
+        // Get the Intent that started this activity and extract the string
+        //Intent intent = getIntent();
+        //String message = intent.getStringExtra(AMazeActivity.EXPLORE_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        //TextView textView = findViewById(R.id.textView);
+        //textView.setText(message);
     }
 }
